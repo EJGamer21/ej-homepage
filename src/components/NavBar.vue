559884f7458e6260e244b8c09dc3ca8cb1889bf4
Enger Jiménez <engersky12@gmail.com>
2019-03-01 21:39:35 -0400

    Initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="nav">
+      <nav class="nav">
+        <router-link :to="{ name: 'home' }">Inicio</router-link> |
+        <router-link :to="{ name: 'recetas-online' }">Recetas Online</router-link> |
+        <router-link :to="{ name: 'sistema-inventario' }">Sistema Invetario</router-link>
+      </nav>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
